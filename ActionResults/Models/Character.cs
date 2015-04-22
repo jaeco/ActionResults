f62a9c757aa2a7d0bfb670b185d8917492657f5c
James <jaeco.89@gmail.com>
2015-04-22 16:00:18 -0600

    Added dictionary and other character attributes

---
+++ ActionResults/Models/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActionResults.Models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int HealthPoints { get; set; }
+
+        public Dictionary<string, Int32> Attributes = new Dictionary<string, Int32>() {
+        {"IQ", 8},{"ME", 9},{"MA", 12},{"PS", 3},{"PP", 4},{"PE", 8},{"PB", 7},{"Spd", 6}};
+
+        public override string ToString()
+        {
+            string temp = "";
+            foreach (KeyValuePair<string, Int32> attribute in Attributes)
+            {
+                temp += " "+attribute.Key+": "+attribute.Value;
+            }
+
+            return "Name: " + Name + "// Level: " + Level + "// HP: " + HealthPoints + "//" + temp;
+        }
+    }
+}
