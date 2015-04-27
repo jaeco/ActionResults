e0a411308b4929ff6c801d32f58f661065315d49
James <jaeco.89@gmail.com>
2015-04-27 14:01:17 -0600

    Changing layout from Table to Div format
    Changing everything to DIVs for formatting instead of tables.

--- ActionResults/Controllers/HomeController.cs
+++ ActionResults/Controllers/HomeController.cs
@@ -9,10 +9,31 @@ namespace ActionResults.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        Character jon = new Character();
+        
+        //Main page
         public ActionResult Index()
         {
-            return View();
+            jon.Name = "John";
+            jon.Level = 12;
+            jon.HealthPoints = 200;
+            return View(jon);
+
         }
+
+        //String page
+        public String StringPage()
+        {
+            return jon.ToString();
+
+        }
+
+        //XML Page
+
+        //JSON Page
+
+        //Error 404
+
+        //Download
     }
 }
