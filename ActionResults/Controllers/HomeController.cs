d9cb9eb7080a9ca79e6a19a1b3a585ed912d67d1
James <jaeco.89@gmail.com>
2015-04-29 23:53:30 -0600

    Finished w/ errors
    XML Serializer would not work correctly, commented out to allow
    functionality

--- ActionResults/Controllers/HomeController.cs
+++ ActionResults/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Xml;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -29,11 +32,41 @@ namespace ActionResults.Controllers
         }
 
         //XML Page
+        //public ActionResult XMLPage()
+        //{
+        //    var serializer = new DataContractSerializer(typeof(Character));
+        //    string xmlString;
+        //    using (var sw = new StringWriter())
+        //    {
+        //        using (var writer = new XmlTextWriter(sw))
+        //        {
+        //            writer.Formatting = Formatting.Indented; // indent the Xml so it's human readable
+        //            serializer.WriteObject(writer, jon);
+        //            writer.Flush();
+        //            xmlString = sw.ToString();
+        //            return this.Content(xmlString, "text/xml");
+        //        }
+        //    }
+        //}
 
         //JSON Page
-
+        public ActionResult JSONPage()
+        {
+            return Json(jon, JsonRequestBehavior.AllowGet);
+        }
         //Error 404
-
+        public ActionResult ErrorPage()
+        {
+            return HttpNotFound("Gurl, you dun got a 404 hunny!");
+        }
         //Download
+        public ActionResult DownloadPage()
+        {
+            var ImagePath = Server.MapPath("~/Images/funny.jpg");
+            Response.AddHeader("Content-Disposition", "attachment;filename=funny.jpg");
+            Response.WriteFile(ImagePath);
+            Response.End();
+            return null;
+        }
     }
 }
--- ActionResults/Models/Character.cs
+++ ActionResults/Models/Character.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Runtime.Serialization;
 
 namespace ActionResults.Models
 {
+    //[DataContract]
     public class Character
     {
+        //[DataMember]
         public string Name { get; set; }
+       // [DataMember]
         public int Level { get; set; }
+       // [DataMember]
         public int HealthPoints { get; set; }
-
+       // [DataMember]
         public Dictionary<string, Int32> Attributes = new Dictionary<string, Int32>() {
         {"IQ", 8},{"ME", 9},{"MA", 12},{"PS", 3},{"PP", 4},{"PE", 8},{"PB", 7},{"Spd", 6}};
 
